9bf8f2f8deea9a8c69bd83ad65fa56547c3284d4
komalessarani <kessara0@live.unc.edu>
2019-04-11 18:54:29 -0400

    got the sound for one key to work!!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
 import React, { Component } from 'react';
 import './App.css';
-import keydown from 'react-keydown';
+import { Howl } from 'howler';
+import note2 from './assets/note2.mp3'; 
 
 class App extends Component {
+
+  arrowKeyListener = (e) => {
+    let key = e.keyCode ? e.keyCode : e.which;
+    var sounda = new Howl({
+      src: note2
+    });
+    switch(key) {
+        case 97:
+        case 65:
+            sounda.play();
+            break;
+        case 115:
+        case 83:
+            
+            break;
+        case 100:
+        case 68:
+            
+            break;
+        case 102:
+        case 70:
+            
+            break;
+        case 106:
+        case 74:
+            
+            break;
+        case 107:
+        case 75:
+            
+            break;
+        case 108:
+        case 76:
+            
+            break;
+        case 59:
+            
+            break;
+        default:
+            console.log(key);
+    }
+};
+
+componentDidMount(){
+    document.addEventListener("keydown", this.arrowKeyListener);
+}
+
+componentWillUnmount(){
+    document.removeEventListener("keydown", this.arrowKeyListener);
+}
 
 render() {
   return (
     <div>
       <h1>Audio Typing</h1>
-           <div class = "buttons">
-             <button id = 'a' class = "A"> A </button>
-             <button id = 's' class = "S"> S </button>
-             <button id = 'd' class = "D"> D </button>
-             <button id = 'f' class = "F"> F </button>
-             <button id = 'j' class = "J"> J </button>
-             <button id = 'k' class = "K"> K </button>
-             <button id = 'l' class = "L"> L </button>
-             <button id = 'm' class = "colon">:</button>
+           <div className = "buttons">
+             <button className = "A"> A </button>
+             <button className = "S"> S </button>
+             <button className = "D"> D </button>
+             <button id = 'f' className = "F"> F </button>
+             <button id = 'j' className = "J"> J </button>
+             <button id = 'k' className = "K"> K </button>
+             <button id = 'l' className = "L"> L </button>
+             <button id = 'colon' className = "colon">;</button>
            </div>
     </div>
   );
