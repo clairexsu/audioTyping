a385d7f483ea5c0e6a25f87945ac633619576a37
komalessarani <kessara0@live.unc.edu>
2019-04-10 20:34:59 -0400

    modified layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import keydown from 'react-keydown';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+render() {
+  return (
+    <div>
+      <h1>Audio Typing</h1>
+           <div class = "buttons">
+             <button id = 'a' class = "A"> A </button>
+             <button id = 's' class = "S"> S </button>
+             <button id = 'd' class = "D"> D </button>
+             <button id = 'f' class = "F"> F </button>
+             <button id = 'j' class = "J"> J </button>
+             <button id = 'k' class = "K"> K </button>
+             <button id = 'l' class = "L"> L </button>
+             <button id = 'm' class = "colon">:</button>
+           </div>
+    </div>
+  );
+}
 }
 
 export default App;
+
+
+
+
