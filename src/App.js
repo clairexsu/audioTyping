dd4baa7d85ba71c7355c8f043a51eaedca2f2263
komalessarani <kessara0@live.unc.edu>
2019-04-21 19:49:26 -0400

    correct sound plays when sounds match

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import { Howl } from 'howler';
 import note2 from './assets/note2.mp3'; 
 import note1 from './assets/note1.mp3';
-import wrong1 from './assets/wrong.mp3'
+import wrong1 from './assets/wrong.mp3';
+import right from './assets/correct.mp3'
 
 class App extends Component {
     constructor(props){
         super(props);
         this.state = {
            checkStart: false,
-           randomNumber: 0,
-           soundArr: [note1, note2]
+           randomIndex: 0,
+           soundArr: [note1, note2],
+           userSound: 0
         }
     }
 
@@ -26,44 +28,75 @@ class App extends Component {
     var wrong = new Howl({
         src: wrong1
     })
-    if(key == 13) {
+
+    var correct = new Howl({
+        src: right
+    })
+
+    //if enter key is pressed then game can start
+    if(key === 13) {
         this.state.checkStart = true;
+        return;
     }
-    else if (this.state.checkStart) {
+    if(this.state.checkStart && key == 102 || key == 70 || key == 106 || key == 74){
+    this.randomIndex = Math.floor(Math.random()*3);
+    console.log(this.randomIndex);
+    switch(this.randomIndex){
+        case 1:
+        sounda.play();
+        break;
+        case 2:
+        soundb.play();
+        break;
+        default:
+        wrong.play();
+    }
+    return;
+}
+    if (this.state.checkStart) {
         switch(key) {
-        case 97:
+        case 97:      //A or a              
         case 65:
             sounda.play();
+            this.userSound = 1;
             break;
-        case 115:
+        case 115:   //S or s
         case 83:
             soundb.play();
+            this.userSound = 2;
             break;
-        case 100:
+        case 100:   //D or d
         case 68:
-            
+        
+            this.userSound = 3;
             break;
-        case 102:
+        case 102:   //F or f
         case 70:
-            
+             this.userSound = 4;
             break;
-        case 106:
+        case 106:   //J or j
         case 74:
-            
+        this.userSound =5;
             break;
-        case 107:
+        case 107:   //K or k
         case 75:
-            
+        this.userSound = 6;
             break;
-        case 108:
+        case 108:   //L or l
         case 76:
-            
+        this.userSound = 7;
             break;
-        case 59:
-            
+        case 59:    //;
+        this.userSound = 8;
             break;
         default:
             wrong.play();
+    }
+    if(this.userSound === this.randomIndex){
+        correct.play();
+    }
+    else{
+        wrong.play();
     }
 }
 };
